0b31a1d1ebceba50ae3fb4e4ade0188b045a9f3e
Jacks8n <cjdxz10@126.com>
2019-01-23 22:01:38 +0800

    Fix path finding

--- StraySouls/DSPath.cs
+++ StraySouls/DSPath.cs
@@ -8,19 +8,32 @@ namespace StraySouls
     {
         private const string REG_PATH_STEAM = @"SOFTWARE\WOW6432Node\Valve\Steam";
         private const string REG_KEYNAME_INSTALL = "InstallPath";
-        private const string FOLDER_PATH_DS3 = @"\steamapps\common\DARK SOULS III\Game\map\mapstudio\";
-        
+
+        private const string FOLDER_PATH_DS3 = @"\steamapps\common\DARK SOULS III\";
+        private const string FOLDER_PATH_MAPSTUDIO = @"Game\map\mapstudio\";
+
         public static string GetDS3MapStudioPath()
         {
-            RegistryKey regKey = Registry.LocalMachine.OpenSubKey(REG_PATH_STEAM);
-            string ds3Path = regKey.GetValue(REG_KEYNAME_INSTALL).ToString() + FOLDER_PATH_DS3;
-
-            while (!Directory.Exists(ds3Path))
+            string ds3Path = string.Empty;
+            try
             {
-                Console.WriteLine("Can't find the folder of dark souls 3 map data.");
-                Console.Write("Type here manually:");
+                RegistryKey regKey = Registry.LocalMachine.OpenSubKey(REG_PATH_STEAM);
+                ds3Path = regKey.GetValue(REG_KEYNAME_INSTALL).ToString() + FOLDER_PATH_DS3 + FOLDER_PATH_MAPSTUDIO;
+            }
+            catch
+            {
+            }
+
+            while (string.IsNullOrWhiteSpace(ds3Path) || !ds3Path.EndsWith(FOLDER_PATH_MAPSTUDIO) || !Directory.Exists(ds3Path))
+            {
+                Console.WriteLine("Can't find the folder of dark souls 3 mapstudio data.");
+                Console.WriteLine("It looks like ..\\DARK SOULS III\\map\\mapstudio\\");
+                Console.Write("Type the full path manually:");
 
                 ds3Path = Console.ReadLine();
+
+                if (!ds3Path.EndsWith("\\"))
+                    ds3Path += "\\";
             }
 
             return ds3Path;
